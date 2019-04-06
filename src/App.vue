76cc7f346048ed4253c4d0a80f1e954af1aeee12
Rémi Laot <remilaot@gmail.com>
2019-04-06 17:08:06 +0200

    navbar and header ok

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
     <div class="header">
-      <h1 class="text-header">Follow'up</h1>
+      <div class="text-header">Follow'up</div>
     </div>
-    <b-navbar id="navBar">
+    <router-view/>
+    <b-navbar id="navbar">
       <b-navbar-nav>
-        <b-nav-item v-for="(section, index) in sections" :key=section.id class="navLink" :to="section.link">
-          <font-awesome-icon :icon="section.icon"/>
-          <div v-if='index <= sections.size' class="ligne_verticale_index">azdazd</div>
+        <b-nav-item v-for="(section) in sections" :key="section.id" :to="section.link" v-bind:class="section.class">
+          <font-awesome-icon class="nav-icon" :icon="section.icon"/>
         </b-nav-item>
       </b-navbar-nav>
     </b-navbar>
-  <router-view/>
   </div>
 </template>
 
@@ -20,11 +19,11 @@
     data() {
       return {
         sections : [
-        { id : 0, icon : "user",     color : "#2c3e50", link : "/compte" },
-        { id : 1, icon : "history",  color : "#2c3e50", link : "/history" },
-        { id : 2, icon : "barcode",  color : "#a8d673", link : "/" },
-        { id : 3, icon : "question", color : "#2c3e50", link : "/premium" },
-        { id : 4, icon : "plus",     color : "#2c3e50", link : "/info" },]
+        { id : 0, class : "secondary", icon : "user",     color : "#2c3e50", link : "/compte" },
+        { id : 1, class : "secondary", icon : "history",  color : "#2c3e50", link : "/history" },
+        { id : 2, class : "main",      icon : "barcode",  color : "#a8d673", link : "/" },
+        { id : 3, class : "secondary", icon : "question", color : "#2c3e50", link : "/premium" },
+        { id : 4, class : "secondary", icon : "plus",     color : "#2c3e50", link : "/info" },]
       }
     },
     components: {}
@@ -33,60 +32,64 @@
 
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
-}
-#navBar{
-	position: fixed;
-	bottom: 0;
-	width: 100%;
-	padding: 0;
-	height: auto;
-  background-color: #F8F8F8;
-	justify-content: center;
-	text-align: center;
-}
-.navLink{
-	display:inline-block;
-	margin-left:5px;
-	padding: 5px 10px;
-	text-decoration:none;
-  vertical-align: center;
-}
-#colorIcon{
-  color: #a8d673;
-}
-.ligne_verticale_index
-{
-  width:5px;
-  border-left: solid 1px #b8bbc1;
-  height: auto;
-}
-.text-header{
-	text-align: center;
-	font-size: 1.5em;
-  padding-top: 4%;
-  padding-bottom: 4%;
-	color: white;
-	letter-spacing: 5px;
-	text-transform: uppercase;
-}
-.header{
-	background-color: #0A5C7C;
-	height: auto;
-}
+
+
+
+  #navbar{
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    padding: 0;
+    background-color: #F8F8F8;
+    justify-content: center;
+  }
+
+  .navbar-nav {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .nav-item{
+    font-size: 1rem;
+    margin-left: 1rem;
+    margin-right: 1rem;
+  }
+
+  .nav-icon {
+    color: #2c3e50;
+  }
+
+  .main .nav-icon {
+    font-size: 2rem;
+  }
+
+  .nav-link.router-link-exact-active .nav-icon {
+    color: #42b983;
+  }
+
+
+
+
+
+  .header{
+    background-color: #0A5C7C;
+  }
+  .text-header{
+    text-align: center;
+    font-size: 1.5rem;
+    padding: 1rem;
+    color: white;
+    letter-spacing: 0.33rem;
+    text-transform: uppercase;
+  }
+
 </style>
